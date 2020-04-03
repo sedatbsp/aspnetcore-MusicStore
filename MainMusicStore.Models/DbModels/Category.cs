2c164eb65bac4d99bb3b06194ba3be5e0e0d6627
Sedat Başpınar <sdtbsp10@gmail.com>
2020-04-04 02:20:30 +0300

    initial migration db and category models added

---
+++ MainMusicStore.Models/DbModels/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MainMusicStore.Models.DbModels
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage ="Bu alan boş geçilemez")]
+        [StringLength(250,MinimumLength = 2,ErrorMessage = "Kategori şartlarına uygun değer giriniz (2-250 characters.)")]
+        public string CategoryName { get; set; }
+
+
+
+    }
+}
