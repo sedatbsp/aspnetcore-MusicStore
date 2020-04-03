6eb0dfe3e2231c5227e693469567bcc535641315
Sedat Başpınar <sdtbsp10@gmail.com>
2020-04-03 15:52:49 +0300

    MainMusicStore N-Tier added and settings updated

---
+++ MainMusicStore.Utility/ProjectConstant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMusicStore.Utility
+{
+    public static class ProjectConstant
+    {
+        public const string ResultNotFound = "Data Not Found // Bulunamadi.";
+        
+    }
+}
