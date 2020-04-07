e4350325288e2e48553caa5c6089e0c4d991bf5b
Sedat Başpınar <sdtbsp10@gmail.com>
2020-04-07 23:26:32 +0300

    repository tier added.

---
+++ MainMusicStore.DataAccess/IMainRepository/ISPCallRepository.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMusicStore.DataAccess.IMainRepository
+{
+    public interface ISPCallRepository : IDisposable
+    {
+        T Single<T>(string procedurName, DynamicParameters parameters = null);
+
+        void Execute(string procedurName, DynamicParameters parameters = null);
+
+        T OneRecord<T>(string procedurName, DynamicParameters parameters = null);
+
+        IEnumerable<T> List<T>(string procedurName, DynamicParameters parameters = null);
+
+        Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string procedurName, DynamicParameters parameters = null);
+
+
+
+    }
+}
---
+++ MainMusicStore.DataAccess/IMainRepository/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMusicStore.DataAccess.IMainRepository
+{
+    public interface IUnitOfWork :IDisposable
+    {
+        ICategoryRepository category { get; }
+        ISPCallRepository sp_call { get; }
+        void Save();
+
+    }
+}
