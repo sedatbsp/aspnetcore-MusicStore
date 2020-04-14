4aa2bee448610757889c3beace6431c95466d5dd
Sedat Başpınar <sdtbsp10@gmail.com>
2020-04-14 23:46:53 +0300

    registration operations

--- MainMusicStore.Utility/ProjectConstant.cs
+++ MainMusicStore.Utility/ProjectConstant.cs
@@ -14,5 +14,12 @@
 
         // ----------------- //
 
+        public const string Role_User_Indi = "Individual Customer";
+        public const string Role_User_Comp = "Company Customer";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
+
+
+
     }
 }
